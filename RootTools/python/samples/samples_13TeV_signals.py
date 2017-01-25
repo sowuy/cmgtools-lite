1e85db0fdad4d73de972d52b2bb97329e840deaf
Constantin Heidegger <constantin.heidegger@cern.ch>
2017-01-25 14:01:20 +0100

    adding sbottom mass and T6bb samples for the Edge

--- RootTools/python/samples/samples_13TeV_signals.py
+++ RootTools/python/samples/samples_13TeV_signals.py
@@ -20,6 +20,9 @@ SMS_T5qqqqVV=kreator.makeMCComponent("SMS_T5qqqqVV","/SMS-T5qqqqVV_TuneCUETP8M1_
 SMS_T6ttWW=kreator.makeMCComponent("SMS_T6ttWW","/SMS-T6ttWW_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM","CMS",".*root",1,True)
 SMS_T6ttHZ=kreator.makeMCComponent("SMS_T6ttHZ","/SMS-T6ttHZ_BR-H_0p6_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUSummer16Fast_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM","CMS",".*root",1,True)
 
+SMS_T6bbllslepton_mSbottom_1000To1500_mLSP_120To1450 = kreator.makeMCComponent("SMS_T6bbllslepton_mSbottom_1000To1500_mLSP_120To1450", "/SMS-T6bbllslepton_mSbottom-1000To1500_mLSP-120To1450_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM","CMS",".*root",1,True)
+SMS_T6bbllslepton_mSbottom_400To950_mLSP_120To140 = kreator.makeMCComponent("SMS_T6bbllslepton_mSbottom_400To950_mLSP_120To140", "/SMS-T6bbllslepton_mSbottom-400To950_mLSP-120To140_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM","CMS",".*root",1,True)
+
 ## EWKino signals 
 #SMS_TChiWZ=kreator.makeMCComponent("SMS_TChiWZ","/SMS-TChiWZ_ZToLL_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v2/MINIAODSIM","CMS",".*root",1,useAAA=True) ## ICHEP
 SMS_TChiWZ=kreator.makeMCComponent("SMS_TChiWZ","/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUSummer16Fast_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM","CMS",".*root",1,True) ## post-ICHEP
@@ -52,6 +55,8 @@ SMS_T1tttt,
 SMS_T5qqqqVV,
 SMS_T6ttWW,
 SMS_T6ttHZ,
+SMS_T6bbllslepton_mSbottom_1000To1500_mLSP_120To1450,
+SMS_T6bbllslepton_mSbottom_400To950_mLSP_120To140,
 SMS_TChiWZ,
 SMS_TChiWH,
 SMS_TChiSlepSnux0p5,
