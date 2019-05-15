cff0db40f0fcc0d0cb06955ab9c465a9f9399434
sesanche <s.scruz.312@gmail.com>
2019-05-15 17:00:13 +0200

    Adding missing stuff

--- TTHAnalysis/python/tools/countJets.py
+++ TTHAnalysis/python/tools/countJets.py
@@ -15,26 +15,27 @@ class CountJets:
 
     def listBranches(self):
         self.branches = [] 
-        for pt in self.jetPts:
+        for sel in self.jetSel:
+            self.branches.append( ('i%s_Recl'%(sel),"I",20)) # indices of jets passing selection
             for var in self.systsJEC:
-                for sel in self.jetSel:
+                for pt in self.jetPts:
                     self.branches.append( 'n%s%d_Recl'%(sel, int(pt))           + self.systsJEC[var])
                     self.branches.append( 'nB%sMedium%d_Recl'%(sel, int(pt)) + self.systsJEC[var])
                     self.branches.append( 'nB%sLoose%d_Recl'%(sel, int(pt))  + self.systsJEC[var])
-                if self.doJetSums:
-                    self.branches.append( 'ht%d_Recl'%(int(pt))  + self.systsJEC[var])
-                    self.branches.append( 'mht%d_Recl'%(int(pt))  + self.systsJEC[var])
+                    if self.doJetSums and sel == 'JetCentral':
+                        self.branches.append( 'ht%d_Recl'%(int(pt))  + self.systsJEC[var])
+                        self.branches.append( 'mht%d_Recl'%(int(pt))  + self.systsJEC[var])
 
-        print self.branches[:]
         return self.branches[:]
     
     def __call__(self,event):
         allret = {} 
         year = '%d'%event.year
         jets = [ j for j in Collection(event,"JetSel","nJetSel")]
-        
         for br in self.branches:
-            allret[br] = 0
+            if type(br) == str: allret[br] = 0
+        for sel in self.jetSel:
+            allret['i%s_Recl'%(sel)] = []
 
         if self.doJetSums:
             _mht  = {}; _ht = {} 
@@ -48,27 +49,33 @@ class CountJets:
                         vl = ROOT.TLorentzVector(); vl.SetPtEtaPhiM( l.pt, 0, l.phi, 0)
                         _mht['%s%d'%(var,int(pt))] = _mht['%s%d'%(var,int(pt))] - vl
                         _ht ['%s%d'%(var,int(pt))] = _ht ['%s%d'%(var,int(pt))] + l.pt
-
-        for pt in self.jetPts:
-            for var in self.systsJEC:
-                for jet in jets:
-                    for sel in self.jetSel:
-                        if not self.jetSel[sel](jet): continue
+        for jet in jets:
+            for sel in self.jetSel:
+                if not self.jetSel[sel](jet): continue
+                allret['i%s_Recl'%sel].append( jets.index(jet))
+                for var in self.systsJEC:
+                    for pt in self.jetPts:
                         if hasattr(jet, 'pt%s'%self.systsJEC[var]) and getattr(jet,'pt%s'%self.systsJEC[var]) > pt or (not var and not hasattr(jet, 'pt%s'%self.systsJEC[var]) and jet.pt > pt): 
                             namJ = 'n%s%d_Recl'%(sel, int(pt)) + self.systsJEC[var]
                             namb = 'nB%sLoose%d_Recl'%(sel, int(pt)) + self.systsJEC[var]
                             namB = 'nB%sMedium%d_Recl'%(sel, int(pt)) + self.systsJEC[var]
                             allret[namJ] = allret[namJ] + 1
-                            print getattr(jet, self.btag)
                             if getattr(jet, self.btag) > self.btagL_thr[year]: allret[namb] = allret[namb] + 1
                             if getattr(jet, self.btag) > self.btagM_thr[year]: allret[namB] = allret[namB] + 1 
-                    if abs(jet.eta) > 2.4: continue 
-                    if hasattr(jet, 'pt%s'%self.systsJEC[var]) and getattr(jet,'pt%s'%self.systsJEC[var]) > pt or (not var and not hasattr(jet, 'pt%s'%self.systsJEC[var]) and jet.pt > pt): 
-                        vj = ROOT.TLorentzVector(); vj.SetPtEtaPhiM( jet.pt, 0, jet.phi, 0)
-                        _mht['%s%d'%(var,int(pt))] = _mht['%s%d'%(var,int(pt))] - vj
-                        _ht ['%s%d'%(var,int(pt))] = _ht ['%s%d'%(var,int(pt))] + jet.pt
+                            if not(self.doJetSums and sel == 'JetCentral'): continue
+                            if hasattr(jet, 'pt%s'%self.systsJEC[var]) and getattr(jet,'pt%s'%self.systsJEC[var]) > pt or (not var and not hasattr(jet, 'pt%s'%self.systsJEC[var]) and jet.pt > pt): 
+                                vj = ROOT.TLorentzVector(); vj.SetPtEtaPhiM( jet.pt, 0, jet.phi, 0)
+                                _mht['%s%d'%(var,int(pt))] = _mht['%s%d'%(var,int(pt))] - vj
+                                _ht ['%s%d'%(var,int(pt))] = _ht ['%s%d'%(var,int(pt))] + jet.pt
 
+                    
                 allret['ht%d_Recl'%(int(pt))  + self.systsJEC[var]]  = _ht ['%s%d'%(var,int(pt))]
                 allret['mht%d_Recl'%(int(pt))  + self.systsJEC[var]] = _mht['%s%d'%(var,int(pt))].Pt()
+
+        for sel in self.jetSel:
+            while len(allret['i%s_Recl'%sel]) < 20:
+                allret['i%s_Recl'%sel].append(0)
+            allret['i%s_Recl'%sel]  = allret['i%s_Recl'%sel][:20]
+
         return allret 
                     
