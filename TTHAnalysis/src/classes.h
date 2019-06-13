0fe628166c5a451a809542350b510fbe50fdb53f
sesanche <s.scruz.312@gmail.com>
2019-06-13 11:32:08 +0200

    First step for tf interface

--- TTHAnalysis/src/classes.h
+++ TTHAnalysis/src/classes.h
@@ -4,12 +4,13 @@
 #include "CMGTools/TTHAnalysis/interface/IgProfHook.h"
 #include "CMGTools/TTHAnalysis/interface/CollectionSkimmer.h"
 #include "CMGTools/TTHAnalysis/interface/CombinedObjectTags.h"
-
+#include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
 namespace {
     struct dictionary {
         SignedImpactParameter sipc;
         DistributionRemapper remapper;
         PdfWeightProducerTool pdfw;
         SetupIgProfDumpHook hook;
+        tensorflow::SessionOptions SessionOptions_;
     };
 }
