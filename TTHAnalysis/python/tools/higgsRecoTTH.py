e971802b94901c677af20c8481e2944be84161ec
sowuy <sophie.wuyckens@student.uclouvain.be>
2019-11-18 09:22:45 +0100

    variables added for HTXS study

--- TTHAnalysis/python/tools/higgsRecoTTH.py
+++ TTHAnalysis/python/tools/higgsRecoTTH.py
@@ -1,9 +1,9 @@
 from __future__ import division
-from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module 
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection as NanoAODCollection
 from CMGTools.TTHAnalysis.tools.nanoAOD.friendVariableProducerTools import declareOutput, writeOutput
 from CMGTools.TTHAnalysis.treeReAnalyzer import Collection as CMGCollection
-from CMGTools.TTHAnalysis.treeReAnalyzer import * 
+from CMGTools.TTHAnalysis.treeReAnalyzer import *
 from CMGTools.TTHAnalysis.tools.genParticleProducer import *
 import ROOT, itertools
 from math import *
@@ -11,16 +11,16 @@ from math import *
 #bTagCut = 0.3093 if year==2016 else 0.3033 if year==2017 else 0.2770
 class HiggsRecoTTH(Module):
     #def __init__(self,label="_Recl",cut_BDT_rTT_score = 0.0, cuts_mW_had = (50.,110.), cuts_mH_vis = (90.,130.), btagDeepCSVveto = 0.4941, doSystJEC=True): #TODO update the values here
-    def __init__(self,label="_Recl",cut_BDT_rTT_score = 0.0, cuts_mW_had = (50.,110.), cuts_mH_vis = (90.,130.), btagDeepCSVveto = 0.2770, doSystJEC=True): 
+    def __init__(self,label="_Recl",cut_BDT_rTT_score = 0.0, cuts_mW_had = (50.,110.), cuts_mH_vis = (90.,130.), btagDeepCSVveto = 0.2770, doSystJEC=True):
         self.label = label
         self.branches = []
         self.systsJEC = {0:"", 1:"_jesTotalUp", -1:"_jesTotalDown"} if doSystJEC else {0:""}
-        for var in self.systsJEC: self.branches.extend(["Hreco_%s%s"%(x,self.systsJEC[var]) for x in ["minDRlj","visHmass","Wmass","lepIdx","j1Idx","j2Idx","pTHvis","matchedpartons","bothmatchedpartons","mismatchedtoptaggedjets","pTHgen"]]) # added new branches here
+        for var in self.systsJEC: self.branches.extend(["Hreco_%s%s"%(x,self.systsJEC[var]) for x in ["minDRlj","visHmass","Wmass","lepIdx","j1Idx","j2Idx","pTHvis","matchedpartons","bothmatchedpartons","mismatchedtoptaggedjets","pTHgen","nPrompt","nHardProcess"]]) # added new branches here
         self.cut_BDT_rTT_score = cut_BDT_rTT_score
         self.cuts_mW_had = cuts_mW_had
         self.cuts_mH_vis = cuts_mH_vis
         self.btagDeepCSVveto = btagDeepCSVveto
-    
+
     # old interface
     def listBranches(self):
         return self.branches
@@ -35,15 +35,15 @@ class HiggsRecoTTH(Module):
         return True
     # code
     def run(self,event,Collection):
-## TODO ##        
+## TODO ##
     # status flag for gen particles
-    # ----------------------------- 
+    # -----------------------------
         statusFlagsMap={
         'isHardProcess' : 7,
         'isPrompt'      : 0
         }
     # define variables and gen collections
-    # ------------------------------------   
+    # ------------------------------------
         #HiggsDaughters = genHiggsDaughtersSelection(genpar) # that is how you define a collection from genproducer, i.e. apply the selection on your collection and it return a filtered collection
         genjet = Collection(event,"GenJet","nGenJet")
         genpar = Collection(event,"GenPart","nGenPart")
@@ -53,55 +53,69 @@ class HiggsRecoTTH(Module):
         LFromWFromT = []
         #gengoodJets = []
         matchedpartons          = 0
-        bothmatchedpartons      = 0 
+        bothmatchedpartons      = 0
         mismatchedtoptaggedjets = 0
         pTHgen = 0
-        
+        nHPrompt = 0
+        nHHardProcess = 0
+
 
     #def isAncestor(a,p): #TODO need to modify this function for my purpose fitting the loop below
         #if a == p:
            #return True
         #for i in xrange(0,p.numberOfMothers()):
             #if isAncestor(a,p.mother(i)):
-               #return True 
+               #return True
         #return False
 
     # loop over gen particles #TODO you can simplify the loop a bit but later, keep it explicit for now
     # -----------------------
         for part in genpar:
             if part.pdgId == 25:
+               """if (part.p4().Pt() > 250. and HTXS_Higgs_pt < 150.):
+                  print("Gen_pt = %f" % (part.p4().Pt()))
+                  print("STXS_pt = %f" % (HTXS_Higgs_pt))
+                  print("Gen_eta = %i" % (part.p4().Eta()))
+                  print("STXS_eta = %i" % (HTXS_Higgs_eta))
+                  print("Gen_mother = %i" % (part.genPartIdxMother))
+                  #print("STXS_mother = %i",HTXS_Higgs_eta)
+                  print("Gen_mother = %i" % (part.status))"""
+
                if part.statusFlags &(1 << statusFlagsMap['isHardProcess']):
                   pTHgen = part.p4().Pt()
+                  nHHardProcess+=1
+               if part.statusFlags &(1 << statusFlagsMap['isPrompt']):
+                  nHPrompt+=1
             if abs(part.pdgId) in range (1,8):
                #print "it is a quark"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this quark is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 25:
                      #print "the mother of this W is a Higgs"
                      QFromWFromH.append(part)
             if abs(part.pdgId) in range (11,18) and part.status == 1:
                #print "it is a lepton"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this lepton is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 25:
                      #print "the mother of this W is a Higgs"
                      LFromWFromH.append(part)
             if abs(part.pdgId) in range (1,8):
                #print "it is a quark"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this quark is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 6:
                      #print "the mother of this W is a Top"
                      QFromWFromT.append(part)
             if abs(part.pdgId) in range (11,18) and part.status == 1:
                #print "it is a lepton"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this lepton is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 6:
                      #print "the mother of this W is a Top"
                      LFromWFromT.append(part)
-    # loop over gen jets 
-    # ------------------ 
+    # loop over gen jets
+    # ------------------
         #for jet in genjet:
             #if not jet.partonFlavour == 5 and not jet.partonFlavour == -5: #TODO that excludes b-jets but it is not necessary
             #if jet.p4().Pt() > 30 and abs(jet.p4().Eta()) < 2.5:  # bit extreme cuts, I think supposed to be 24 and 2.4
@@ -113,10 +127,10 @@ class HiggsRecoTTH(Module):
         leps   = Collection(event,"LepGood","nLepGood")
         lepsFO = [leps[ileps[i]] for i in xrange(nFO)]
         jets   = [x for x in Collection(event,"JetSel"+self.label,"nJetSel"+self.label)]
-        ret    = {} 
+        ret    = {}
 
         for var in self.systsJEC:
-            score = getattr(event,"BDThttTT_eventReco_mvaValue%s"%self.systsJEC[var]) 
+            score = getattr(event,"BDThttTT_eventReco_mvaValue%s"%self.systsJEC[var])
             candidates=[]
             if score>self.cut_BDT_rTT_score:
 
@@ -129,7 +143,7 @@ class HiggsRecoTTH(Module):
                     # need to remove #TODO
                     # --------------
                     #if not len(lepsFO)==2: continue #these are redundant cuts when you do the plotting
-                    #if not lepsFO[0].charge==lepsFO[1].charge: continue 
+                    #if not lepsFO[0].charge==lepsFO[1].charge: continue
                     for _j1,_j2,j1,j2 in [(jets.index(x1),jets.index(x2),x1.p4(),x2.p4()) for x1,x2 in itertools.combinations(jetsNoTopNoB,2)]:
                         j1.SetPtEtaPhiM(getattr(jets[jets.index(x1)],'pt%s'%self.systsJEC[var]),j1.Eta(), j1.Phi(), j1.M())
                         j2.SetPtEtaPhiM(getattr(jets[jets.index(x2)],'pt%s'%self.systsJEC[var]),j2.Eta(), j2.Phi(), j2.M())
@@ -160,14 +174,14 @@ class HiggsRecoTTH(Module):
                            mismatchedtoptaggedjets +=1 #only with respect to the hadronic top where the W is going to qq and this is what I am matching here
             best = min(candidates) if len(candidates) else None
             if best:
-               jetmat1 = jets[best[4]] 
+               jetmat1 = jets[best[4]]
                jetmat2 = jets[best[5]]
-	       for quark in QFromWFromH: 
+	       for quark in QFromWFromH:
 	           if deltaR(quark.p4().Eta(),quark.p4().Phi(), jetmat1.p4().Eta(),jetmat1.p4().Phi()) < 0.3 or deltaR(quark.p4().Eta(),quark.p4().Phi(), jetmat2.p4().Eta(),jetmat2.p4().Phi()) < 0.3:
 	              matchedpartons +=1
 	           elif deltaR(quark.p4().Eta(),quark.p4().Phi(), jetmat1.p4().Eta(),jetmat1.p4().Phi()) < 0.3 and deltaR(quark.p4().Eta(),quark.p4().Phi(), jetmat2.p4().Eta(),jetmat2.p4().Phi()) < 0.3:
 		        bothmatchedpartons +=1
-            else: pass   
+            else: pass
             ret["Hreco_minDRlj%s"                   %self.systsJEC[var]] = best[0 ] if best else -99
             ret["Hreco_visHmass%s"                  %self.systsJEC[var]] = best[1 ] if best else -99
             ret["Hreco_Wmass%s"                     %self.systsJEC[var]] = best[2 ] if best else -99
@@ -175,8 +189,11 @@ class HiggsRecoTTH(Module):
             ret["Hreco_j1Idx%s"                     %self.systsJEC[var]] = best[4 ] if best else -99
             ret["Hreco_j2Idx%s"                     %self.systsJEC[var]] = best[5 ] if best else -99
             ret["Hreco_pTHvis%s"                    %self.systsJEC[var]] = best[6 ] if best else -99
-            ret["Hreco_matchedpartons%s"            %self.systsJEC[var]] = matchedpartons if best else -99 
+            ret["Hreco_matchedpartons%s"            %self.systsJEC[var]] = matchedpartons if best else -99
             ret["Hreco_bothmatchedpartons%s"        %self.systsJEC[var]] = bothmatchedpartons if best else -99
-            ret["Hreco_pTHgen%s"                    %self.systsJEC[var]] = pTHgen 
-            ret["Hreco_mismatchedtoptaggedjets%s"   %self.systsJEC[var]] = mismatchedtoptaggedjets 
+            ret["Hreco_pTHgen%s"                    %self.systsJEC[var]] = pTHgen
+            ret["Hreco_mismatchedtoptaggedjets%s"   %self.systsJEC[var]] = mismatchedtoptaggedjets
+            ret["Hreco_nPrompt%s"                   %self.systsJEC[var]] = nHPrompt
+            ret["Hreco_nHardProcess%s"              %self.systsJEC[var]] = nHHardProcess
+
         return ret
--- TTHAnalysis/python/tools/higgsRecoTTH_NoTopTag.py
+++ TTHAnalysis/python/tools/higgsRecoTTH_NoTopTag.py
@@ -1,25 +1,25 @@
 from __future__ import division
-from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module 
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection as NanoAODCollection
 from CMGTools.TTHAnalysis.tools.nanoAOD.friendVariableProducerTools import declareOutput, writeOutput
 from CMGTools.TTHAnalysis.treeReAnalyzer import Collection as CMGCollection
-from CMGTools.TTHAnalysis.treeReAnalyzer import * 
+from CMGTools.TTHAnalysis.treeReAnalyzer import *
 from CMGTools.TTHAnalysis.tools.genParticleProducer import *
 import ROOT, itertools
 from math import *
 #bTagCut = 0.3093 if year==2016 else 0.3033 if year==2017 else 0.2770
 class HiggsRecoTTH(Module):
-    #def __init__(self,label="_Recl",cut_BDT_rTT_score = 0.0, cuts_mW_had = (50.,110.), cuts_mH_vis = (90.,130.), btagDeepCSVveto = 0.4941, doSystJEC=True): 
+    #def __init__(self,label="_Recl",cut_BDT_rTT_score = 0.0, cuts_mW_had = (50.,110.), cuts_mH_vis = (90.,130.), btagDeepCSVveto = 0.4941, doSystJEC=True):
     def __init__(self,label="_Recl",cut_BDT_rTT_score = 0.0, cuts_mW_had = (50.,110.), cuts_mH_vis = (90.,130.), btagDeepCSVveto = 0.2770, doSystJEC=True): #TODO update the values here
         self.label = label
         self.branches = []
         self.systsJEC = {0:"", 1:"_jesTotalUp", -1:"_jesTotalDown"} if doSystJEC else {0:""}
-        for var in self.systsJEC: self.branches.extend(["Hreco_%s%s"%(x,self.systsJEC[var]) for x in ["minDRlj","visHmass","Wmass","lepIdx","j1Idx","j2Idx","pTHvis","matchedpartons","bothmatchedpartons","pTHgen"]]) # added new branches here
+        for var in self.systsJEC: self.branches.extend(["Hreco_%s%s"%(x,self.systsJEC[var]) for x in ["minDRlj","visHmass","Wmass","lepIdx","j1Idx","j2Idx","pTHvis","matchedpartons","bothmatchedpartons","pTHgen","etaHgen","statusHgen","motherHgen"]]) # added new branches here
         self.cut_BDT_rTT_score = cut_BDT_rTT_score
         self.cuts_mW_had = cuts_mW_had
         self.cuts_mH_vis = cuts_mH_vis
         self.btagDeepCSVveto = btagDeepCSVveto
-    
+
     # old interface
     def listBranches(self):
         return self.branches
@@ -34,15 +34,15 @@ class HiggsRecoTTH(Module):
         return True
     # code
     def run(self,event,Collection):
-## TODO ##        
+## TODO ##
     # status flag for gen particles
-    # ----------------------------- 
+    # -----------------------------
         statusFlagsMap={
         'isHardProcess' : 7,
         'isPrompt'      : 0
         }
     # define variables and gen collections
-    # ------------------------------------   
+    # ------------------------------------
         #HiggsDaughters = genHiggsDaughtersSelection(genpar) # that is how you define a collection from genproducer, i.e. apply the selection on your collection and it return a filtered collection
         genjet = Collection(event,"GenJet","nGenJet")
         genpar = Collection(event,"GenPart","nGenPart")
@@ -52,53 +52,59 @@ class HiggsRecoTTH(Module):
         LFromWFromT = []
         #gengoodJets = []
         matchedpartons          = 0
-        bothmatchedpartons      = 0 
+        bothmatchedpartons      = 0
         pTHgen                  = 0
+        etaHgen                 = 0
+        motherHgen              = 0
+        statusHgen              = 0
 
     #def isAncestor(a,p): #TODO need to modify this function for my purpose fitting the loop below
         #if a == p:
            #return True
         #for i in xrange(0,p.numberOfMothers()):
             #if isAncestor(a,p.mother(i)):
-               #return True 
+               #return True
         #return False
 
     # loop over gen particles #TODO you can simplify the loop a bit but later, keep it explicit for now
     # -----------------------
         for part in genpar:
             if part.pdgId == 25:
-               if part.statusFlags &(1 << statusFlagsMap['isHardProcess']): 
+               if part.statusFlags &(1 << statusFlagsMap['isHardProcess']):
                   pTHgen = part.p4().Pt()
+                  motherHgen = part.genPartIdxMother
+                  statusHgen = part.statusFlags
+                  etaHgen = part.p4().Eta()
             if abs(part.pdgId) in range (1,8):
                #print "it is a quark"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this quark is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 25:
                      #print "the mother of this W is a Higgs"
                      QFromWFromH.append(part)
             if abs(part.pdgId) in range (11,18) and part.status == 1:
                #print "it is a lepton"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this lepton is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 25:
                      #print "the mother of this W is a Higgs"
                      LFromWFromH.append(part)
             if abs(part.pdgId) in range (1,8):
                #print "it is a quark"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this quark is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 6:
                      #print "the mother of this W is a Top"
                      QFromWFromT.append(part)
             if abs(part.pdgId) in range (11,18) and part.status == 1:
                #print "it is a lepton"
-               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24: 
+               if part.genPartIdxMother >= 0 and abs(genpar[part.genPartIdxMother].pdgId) == 24:
                   #print "the mother of this lepton is W+ or W-"
                   if abs(genpar[genpar[part.genPartIdxMother].genPartIdxMother].pdgId) == 6:
                      #print "the mother of this W is a Top"
                      LFromWFromT.append(part)
-    # loop over gen jets 
-    # ------------------ 
+    # loop over gen jets
+    # ------------------
         #for jet in genjet:
             #if not jet.partonFlavour == 5 and not jet.partonFlavour == -5: #TODO that excludes b-jets but it is not necessary
             #if jet.p4().Pt() > 30 and abs(jet.p4().Eta()) < 2.5:  # bit extreme cuts, I think supposed to be 24 and 2.4
@@ -110,11 +116,11 @@ class HiggsRecoTTH(Module):
         leps   = Collection(event,"LepGood","nLepGood")
         lepsFO = [leps[ileps[i]] for i in xrange(nFO)]
         jets   = [x for x in Collection(event,"JetSel"+self.label,"nJetSel"+self.label)]
-        ret    = {} 
+        ret    = {}
 
         for var in self.systsJEC:
             score = 1
-            #score = getattr(event,"BDThttTT_eventReco_mvaValue%s"%self.systsJEC[var]) 
+            #score = getattr(event,"BDThttTT_eventReco_mvaValue%s"%self.systsJEC[var])
             candidates=[]
             if score>self.cut_BDT_rTT_score:
 
@@ -126,7 +132,7 @@ class HiggsRecoTTH(Module):
                 jetsNoTopNoB = [j for i,j in enumerate(jets) if j.btagDeepB<self.btagDeepCSVveto]
                 for _lep,lep in [(ix,x.p4()) for ix,x in enumerate(lepsFO)]:
                     if not len(lepsFO)==2: continue #these are redundant cuts when you do the plotting
-                    if not lepsFO[0].charge==lepsFO[1].charge: continue 
+                    if not lepsFO[0].charge==lepsFO[1].charge: continue
                     for _j1,_j2,j1,j2 in [(jets.index(x1),jets.index(x2),x1.p4(),x2.p4()) for x1,x2 in itertools.combinations(jetsNoTopNoB,2)]:
                         j1.SetPtEtaPhiM(getattr(jets[jets.index(x1)],'pt%s'%self.systsJEC[var]),j1.Eta(), j1.Phi(), j1.M())
                         j2.SetPtEtaPhiM(getattr(jets[jets.index(x2)],'pt%s'%self.systsJEC[var]),j2.Eta(), j2.Phi(), j2.M())
@@ -151,7 +157,7 @@ class HiggsRecoTTH(Module):
                                  #print "both detector level jets match with both true ones --> counting it"
                                  #bothmatchedjets +=1
             best = min(candidates) if len(candidates) else None
-            if best: 
+            if best:
                jetmat1 = jets[best[4]]
                jetmat2 = jets[best[5]]
                for quark in QFromWFromH:
@@ -167,6 +173,9 @@ class HiggsRecoTTH(Module):
             ret["Hreco_j2Idx%s"                     %self.systsJEC[var]] = best[5 ] if best else -99
             ret["Hreco_pTHvis%s"                    %self.systsJEC[var]] = best[6 ] if best else -99
             ret["Hreco_matchedpartons%s"            %self.systsJEC[var]] = matchedpartons if best else -99
-            ret["Hreco_bothmatchedpartons%s"        %self.systsJEC[var]] = bothmatchedpartons if best else -99 
+            ret["Hreco_bothmatchedpartons%s"        %self.systsJEC[var]] = bothmatchedpartons if best else -99
             ret["Hreco_pTHgen%s"                    %self.systsJEC[var]] = pTHgen
+            ret["Hreco_etaHgen%s"                   %self.systsJEC[var]] = etaHgen
+            ret["Hreco_motherHgen%s"                %self.systsJEC[var]] = motherHgen
+            ret["Hreco_statusHgen%s"                %self.systsJEC[var]] = statusHgen
         return ret
